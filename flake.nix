fb4daa7f767105f926ecf78899dbe26e5ace95a8
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:52 -0300

    chore: re index awesome_rmdir

--- flake.nix
+++ flake.nix
@@ -7,6 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."awesome_rmdir-develop".dir   = "develop";
+  inputs."awesome_rmdir-develop".owner = "nim-nix-pkgs";
+  inputs."awesome_rmdir-develop".ref   = "master";
+  inputs."awesome_rmdir-develop".repo  = "awesome_rmdir";
+  inputs."awesome_rmdir-develop".type  = "github";
+  inputs."awesome_rmdir-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."awesome_rmdir-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."awesome_rmdir-master".dir   = "master";
+  inputs."awesome_rmdir-master".owner = "nim-nix-pkgs";
+  inputs."awesome_rmdir-master".ref   = "master";
+  inputs."awesome_rmdir-master".repo  = "awesome_rmdir";
+  inputs."awesome_rmdir-master".type  = "github";
+  inputs."awesome_rmdir-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."awesome_rmdir-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."awesome_rmdir-v0_2_0".dir   = "v0_2_0";
+  inputs."awesome_rmdir-v0_2_0".owner = "nim-nix-pkgs";
+  inputs."awesome_rmdir-v0_2_0".ref   = "master";
+  inputs."awesome_rmdir-v0_2_0".repo  = "awesome_rmdir";
+  inputs."awesome_rmdir-v0_2_0".type  = "github";
+  inputs."awesome_rmdir-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."awesome_rmdir-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."awesome_rmdir-v0_2_1".dir   = "v0_2_1";
+  inputs."awesome_rmdir-v0_2_1".owner = "nim-nix-pkgs";
+  inputs."awesome_rmdir-v0_2_1".ref   = "master";
+  inputs."awesome_rmdir-v0_2_1".repo  = "awesome_rmdir";
+  inputs."awesome_rmdir-v0_2_1".type  = "github";
+  inputs."awesome_rmdir-v0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."awesome_rmdir-v0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
